049a99e5acb84bae8d807aae06428480011d8d69
Raul Ferrando <raulferrando99@gmail.com>
2023-03-01 20:52:43 +0100

    create a custom filter specifyng the target field

--- signal_server/src/log.rs
+++ signal_server/src/log.rs
@@ -1,28 +1,32 @@
-use tracing_subscriber::{prelude::*, fmt, EnvFilter};
+use tracing_subscriber::{prelude::*, fmt, filter};
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
 
 pub fn start_logger() {
+    let stdout_filter = filter::FilterFn::new(|metadata| {
+        metadata.target() == "signal_server"
+    });
+    let file_filter = filter::FilterFn::new(|metadata| {
+        metadata.target() == "signal_server"
+    });
 
     let stdout_logger = fmt::layer()
         .with_target(false)
         .with_line_number(true)
-        .with_file(true);
+        .with_file(true)
+        .with_filter(stdout_filter);
 
-    let stdout_filter = EnvFilter::try_from_default_env()
-        .or_else(|_| EnvFilter::try_new("debug"))
-        .unwrap();
-
+    
     let file_appender = RollingFileAppender::new(Rotation::DAILY, "logs", "signal_server.log");
     let debug_log = tracing_subscriber::fmt::layer()
         .with_target(true)
         .with_line_number(true)
         .with_file(true)
-        .with_writer(file_appender);
+        .with_writer(file_appender)
+        .with_filter(file_filter);
 
     tracing_subscriber::registry()
         .with(
             stdout_logger
-                .with_filter(stdout_filter)
                 .and_then(debug_log)
         )
         .init();
